b1aba3b95bb1a1067873b36b866aa3df71802a4f
Rafael Fernandes de Oliveira <rafael.oliveira@ativainvestimentos.com.br>
2021-08-18 18:48:39 -0300

    Definindo o layout do item de tarefa

---
+++ projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="task-grid">
+        <template>
+            <!-- para que não tenha duplicação no nome, 
+            uso o mesmo como chave -->
+            <Task v-for="task in tasks" :key="task.name" :task="task" />
+        </template>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue';
+
+export default {
+    components: { Task },
+    props: {
+        tasks: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
