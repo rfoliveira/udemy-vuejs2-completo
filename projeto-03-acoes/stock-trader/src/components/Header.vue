161c5131ea5bb294c75bbdc655dfeee7f40c7794
Rafael Fernandes de Oliveira <rafael.oliveira@ativainvestimentos.com.br>
2021-09-20 22:06:08 -0300

    Ajuste no menu ok. Precisa ajustar a tela de ações

--- projeto-03-acoes/stock-trader/src/components/Header.vue
+++ projeto-03-acoes/stock-trader/src/components/Header.vue
@@ -1,39 +1,42 @@
 <template>
-    <!-- <v-card flat> -->
-        <v-toolbar app>
+<!-- No curso o intrutor fez de outra forma
+mas a versão dele era outra -->
+    <v-card>
+        <v-toolbar>
             <v-toolbar-title class="headline text-uppercase mr-4">
                 <span>Stock</span>
                 <span class="font-weight-light">Trader</span>
             </v-toolbar-title>
             <v-toolbar-items>
-                <v-btn flat to="/">Início</v-btn>
-                <v-btn flat to="/portifolio">Portifólio</v-btn>
-                <v-btn flat to="/stocks">Ações</v-btn>
+                <v-btn text to="/">Início</v-btn>
+                <v-btn text to="/portifolio">Portifólio</v-btn>
+                <v-btn text to="/stocks">Ações</v-btn>
             </v-toolbar-items>
 
             <v-spacer></v-spacer>
 
             <v-toolbar-items>
-                <v-btn flat>Finalizar Dia</v-btn>
+                <v-btn text>Finalizar o dia</v-btn>
                 <v-menu offset-y>
-                    <v-btn flat slot="activator">Salvar & Carregar</v-btn>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn text v-bind="attrs" v-on="on">Salvar & Carregar</v-btn>
+                    </template>
                     <v-list>
-                        <v-list-tile>
-                            <v-list-tile-title>Salvar Dados</v-list-tile-title>
-                        </v-list-tile>
-                        <v-list-tile>
-                            <v-list-tile-title>Carregar Dados</v-list-tile-title>
-                        </v-list-tile>
+                        <v-list-item>Salvar Dados</v-list-item>
+                        <v-list-item>Carregar Dados</v-list-item>
                     </v-list>
+                    <!-- Dá no mesmo se fizer 
+                    <v-list><v-list-item><v-list-item-title>
+                        ... 
+                    </v-list-item-title></v-list-item></v-list>-->
                 </v-menu>
             </v-toolbar-items>
         </v-toolbar>
-    <!-- </v-card> -->
+    </v-card>
 </template>
 
 <script>
 export default {
-    
 }
 </script>
 
--- projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -1,5 +1,36 @@
 <template>
-    <h1>Ação</h1>
+    <!-- Forma do instrutor que não fica direito -->
+    <!-- <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>NOME <small>(Preço: PREÇO)</small></strong>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="Quantidade" type="number"></v-text-field>
+                <v-btn class="green darken-3 white--text">Comprar</v-btn>
+            </v-container>
+        </v-card>
+    </v-flex> -->
+    <v-flex class="pa-3">
+        <v-card class="d-flex green darken-3 white--text text-h5" outlined tile>
+            <v-card-title>
+                <strong>NOME <small>(Preço: PREÇO)</small></strong>
+            </v-card-title>
+        </v-card>
+
+        <v-card>
+            <v-container>
+                <v-text-field label="Quantidade" type="number"></v-text-field>
+                <v-btn class="green darken-3 white--text">Comprar</v-btn>
+            </v-container>
+        </v-card>
+
+        
+    </v-flex>
+    
+    
 </template>
 
 <script>
