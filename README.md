e53eec65dd9bc5ae8db087dda51e3cf1758cd8b2
Rafael Fernandes de Olveira <rafaelolivei@gmail.com>
2023-05-08 00:10:30 -0300

    Repositório limpo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Curso Vue JS 2 - O Guia Completo (incl. Vue Router &amp; Vuex)
+
+## REFEITO PARA O VUEJS 3
--- vuex/vuex-exercicios/src/store/getters.js
+++
@@ -1,3 +0,0 @@
-// Exemplo de organização de arquivo com todos os getters
-export const getNome = state => state.nome
-export const getNomeCompleto = state => `${state.nome} ${state.sobrenome}`
